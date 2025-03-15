25ad745918528a54945f8b05d319ccfc29c6e32c
hao-1392009 <havenchen2020@gmail.com>
2025-03-15 23:30:52 +0800

    done

--- student_agent.py
+++ student_agent.py
@@ -2,7 +2,130 @@
 import numpy as np
 import pickle
 import random
-import gym
+from enum import IntEnum
+
+
+class Action(IntEnum):
+    MOVE_DOWN = 0
+    MOVE_UP = 1
+    MOVE_RIGHT = 2
+    MOVE_LEFT = 3
+    PICK_UP = 4
+    DROP_OFF = 5
+
+    @staticmethod
+    def get_space_size():
+        return 6
+
+    @staticmethod
+    def sample():
+        return random.choice(list(Action))
+
+    @staticmethod
+    def is_movement(action):
+        if action is None:
+            return False
+        return 0 <= action <= 3
+
+stations = [None] * 4
+passenger_pos = None
+can_be_passenger = [True] * 4
+destination_pos = None
+can_be_destination = [True] * 4
+carrying = False
+num_visited = None
+
+def get_state(obs, prev_action, should_reset=False):
+    # obs = taxi_row,            taxi_col,            self.stations[0][0], self.stations[0][1],
+    #       self.stations[1][0], self.stations[1][1], self.stations[2][0], self.stations[2][1],
+    #       self.stations[3][0], self.stations[3][1], obstacle_north,      obstacle_south,
+    #       obstacle_east,       obstacle_west,       passenger_look,      destination_look
+
+    global stations, passenger_pos, can_be_passenger, destination_pos, can_be_destination, carrying, num_visited
+
+    taxi_row, taxi_col, _,_,_,_,_,_,_,_, obstacle_north, obstacle_south, \
+        obstacle_east, obstacle_west, passenger_look, destination_look = obs
+
+    for i in range(4):
+        if (obs[2 * i + 2], obs[2 * i + 3]) != stations[i]:
+            # New episode started. Reset everything.
+            should_reset = True
+            break
+
+    if should_reset:
+        stations = [(obs[2 * i + 2], obs[2 * i + 3]) for i in range(4)]
+        passenger_pos = None
+        can_be_passenger = [True] * 4
+        destination_pos = None
+        can_be_destination = [True] * 4
+        carrying = False
+        num_visited = [[0 for j in range(10)] for i in range(10)]
+        prev_action = None
+
+
+    rel_positions = [(r - taxi_row, c - taxi_col) for r, c in stations]
+    rel_distances = [abs(r) + abs(c) for r, c in rel_positions]
+
+    if passenger_pos is None:
+        if passenger_look:
+            can_be_passenger = [can_be_passenger[i] and rel_distances[i] <= 1 for i in range(4)]
+        else:
+            can_be_passenger = [can_be_passenger[i] and rel_distances[i] > 1 for i in range(4)]
+    if destination_pos is None:
+        if destination_look:
+            can_be_destination = [can_be_destination[i] and rel_distances[i] <= 1 for i in range(4)]
+        else:
+            can_be_destination = [can_be_destination[i] and rel_distances[i] > 1 for i in range(4)]
+
+    if passenger_pos is None and sum(can_be_passenger) == 1:
+        index = can_be_passenger.index(True)
+        passenger_pos = stations[index]
+        can_be_destination[index] = False
+    if destination_pos is None and sum(can_be_destination) == 1:
+        index = can_be_destination.index(True)
+        destination_pos = stations[index]
+        can_be_passenger[index] = False
+    if passenger_pos is None and sum(can_be_passenger) == 1:
+        index = can_be_passenger.index(True)
+        passenger_pos = stations[index]
+
+
+    if prev_action == Action.PICK_UP and (taxi_row, taxi_col) == passenger_pos:
+        carrying = True
+    elif prev_action == Action.DROP_OFF and carrying:
+        carrying = False
+        passenger_pos = (taxi_row, taxi_col)
+        if (taxi_row, taxi_col) == destination_pos:
+            stations[0] = None  # make sure next call to get_state() will reset
+
+
+    if not carrying and passenger_pos is not None:
+        target_rel = (passenger_pos[0] - taxi_row, passenger_pos[1] - taxi_col)
+    elif carrying and destination_pos is not None:
+        target_rel = (destination_pos[0] - taxi_row, destination_pos[1] - taxi_col)
+    else:
+        index_closest = None
+        for i in range(4):
+            if (can_be_destination[i] if carrying else can_be_passenger[i])\
+                and (index_closest is None or rel_distances[i] < rel_distances[index_closest]):
+                index_closest = i
+        target_rel = rel_positions[index_closest]
+
+    target_rel = (np.sign(target_rel[0]), np.sign(target_rel[1]))
+
+
+    if Action.is_movement(prev_action):
+        num_visited[taxi_row][taxi_col] += 1
+    num_visited_n = num_visited[taxi_row - 1][taxi_col] // 3 if not obstacle_north else -1
+    num_visited_s = num_visited[taxi_row + 1][taxi_col] // 3 if not obstacle_south else -1
+    num_visited_e = num_visited[taxi_row][taxi_col + 1] // 3 if not obstacle_east else -1
+    num_visited_w = num_visited[taxi_row][taxi_col - 1] // 3 if not obstacle_west else -1
+
+    return (target_rel, num_visited_n, num_visited_s, num_visited_e, num_visited_w, carrying)
+
+action = None
+with open("q_table.pkl", "rb") as f:
+    q_table = pickle.load(f)
 
 def get_action(obs):
 
@@ -12,17 +135,12 @@ def get_action(obs):
     #       To prevent crashes, implement a fallback strategy for missing keys.
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
 
-    action = [0, 1, 2, 3, 4, 5]
-    if obs[10]==1:
-        action.remove(1)
-    if obs[13]==1:
-        action.remove(3)
-    if obs[12]==1:
-        action.remove(2)
-    if obs[11]==1:
-        action.remove(0)
-    if obs[14]!=1:
-        action.remove(4)
-    if obs[15]!=1:
-        action.remove(5)
-    return random.choice(action)
+    global action
+
+    state = get_state(obs, action)
+    if state not in q_table.keys() or np.random.rand() < 0.1:
+        action = Action.sample()
+    else:
+        action = np.argmax(q_table[state])
+
+    return action
